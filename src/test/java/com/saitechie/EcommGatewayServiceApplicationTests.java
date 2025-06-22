a46a4331a9603589987757fe3a8c4dc90f1ac424
vojjalasaikumar-techie <saikumar.vojjala5444@gmail.com>
2025-06-22 19:19:43 +0530

    api gateway configured for service registy

---
+++ src/test/java/com/saitechie/EcommGatewayServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.saitechie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+class EcommGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
